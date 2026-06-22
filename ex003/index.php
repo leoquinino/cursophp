eb858ef21a80abe4d0caf1cee96adcd785670dd8
leoquinino <leonardo.quinino@gmail.com>
2026-06-22 16:31:30 -0300

    Ex003 - Variaveis e Constantes

---
+++ ex003/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Estudando PHP - Ex003 - Variaveis e Constantes</title>
+</head>
+<body>
+    <?php 
+    $nome = "Leonardo";
+    $sobrenome = "Silva";
+    const PAIS = "Brasil";
+    echo 'Olá, ' . $nome . ' ' . $sobrenome . '!.<br>';
+    echo ' Você é do ' . PAIS . '.';
+    ?>
+</body>
+</html>
